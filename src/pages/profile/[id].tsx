af048d20e4656e64aa499576b0904e8760c9993f
Your Name <you@example.com>
2020-09-03 09:50:51 +0300

    WIP

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,17 +1,58 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React, { useRef, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import axios from 'axios';
 
-export default function Profile(postData) {
-  const router = useRouter();
-  return (
-    <div>
-      Profile details
-    </div>
+const addProduct = async (title: string, content: string) => {
+  const response = await axios.post('https://old-crab-12.loca.lt/feed/product',
+    {
+      title,
+      content
+    },
+    {
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRpbWEuc3RyYXR1bGF0OTkxQGdtYWlsLmNvbSIsInVzZXJJZCI6IjVmNGZhMDIxNmVjZTYwZDA1ZTFjYWYyZiIsImlhdCI6MTU5OTA1Mzg1NywiZXhwIjoxNTk5MjI2NjU3fQ.QgITlakldl_4jpMZSfRNo8OBY5IVcYWVGJ-jdLemYss'
+      }
+    }
   )
 }
 
+export default function Profile({ products }) {
+  const titleInput = useRef<HTMLInputElement>(null);
+  const contentInput = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+  console.log(products)
+  return (
+    <div>
+      {products.products.map((product, key) => (
+        <div key={key}>{product.title}</div>
+      ))}
+      Profile details
+      <p>
+        Title
+        <input type="text" ref={titleInput} />
+      </p>
+      <p>
+        Content
+        <input type="text" ref={contentInput} />
+      </p>
+      <button onClick={() => {
+        addProduct(
+          titleInput.current!.value,
+          contentInput.current!.value
+        )
+      }}>
+        Upload
+      </button>
+    </div>
+  )
+}
+const getProfileProducts = async () => {
+  const response = await axios.get('https://old-crab-12.loca.lt/feed/products')
+  return response.data
+}
+
 export async function getStaticPaths() {
+
   return {
     paths: [
       { params: { id: "1" } }
@@ -22,10 +63,12 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(ctx) {
   //request
-  const requestResponse = 1;
+  console.log('gettstaticpropss 1')
+  const data = await getProfileProducts();
+  console.log(data)
   return {
     props: {
-      postData: requestResponse
+      products: data
     }
   }
 }
