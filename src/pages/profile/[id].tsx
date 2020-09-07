49fd0440b674b8700a24af7a09545515c70bf31a
Your Name <you@example.com>
2020-09-07 09:50:08 +0300

    added profile wip

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,28 +1,34 @@
 import React, { useRef, useEffect } from 'react';
 import { getProfileProducts, addProduct } from 'api/profile';
+import { Row, Col } from 'antd';
+import style from './profile.module.css'
+import Link from 'next/Link';
 
 export default function Profile({ products }) {
   const titleInput = useRef<HTMLInputElement>(null);
   const contentInput = useRef<HTMLInputElement>(null);
+  console.log(products)
   return (
     <div>
-      {products.products.map((product, key) => (
-        <div key={key}>{product.title}</div>
-      ))}
-      Profile details
-      <p>
-        Title
-        <input type="text" ref={titleInput} />
-      </p>
-      <p>
-        Content
-        <input type="text" ref={contentInput} />
-      </p>
-      <button onClick={() => {
-      }}>
-        Upload
-      </button>
-    </div>
+      <Row justify="center" >
+        {products.products.map((product, key) => (
+          <Col
+            xs={{ span: 8 }}
+            sm={{ span: 8 }}
+            md={{ span: 8 }}
+            lg={{ span: 4 }}
+            xl={{ span: 4 }}
+          >
+            <Link href='/product/[id]' as={`/product/${product._id}`}>
+              <a >
+                <img src={require('./logo.jpg')} className={style.image} />
+              </a>
+            </Link>
+            <p className={style.price}>${product.price}</p>
+          </Col>
+        ))}
+      </Row>
+    </div >
   )
 }
 
