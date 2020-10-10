9ad4271105b76590605a7849acaf3d7303466fef
Your Name <you@example.com>
2020-10-10 18:38:54 +0300

    added handlePermission in profile button

--- src/components/ProfileButton/ProfileButton.tsx
+++ src/components/ProfileButton/ProfileButton.tsx
@@ -1,20 +1,27 @@
 import React, { useContext } from 'react'
 import { AppContext, ContextProps } from 'context/AppContext'
 import { User } from 'interfaces/interfaces';
-import { Router } from 'next/router';
-import Link from 'next/Link';
+import Router from 'next/router';
+import { getUserInfo } from 'context/queries';
+import style from '../HeaderLayout/HeaderLayout.module.css';
 
 export default function ProfileButton() {
   const ctx: ContextProps | null = useContext(AppContext);
   if (!ctx) {
     throw new Error('You probably forgot to put <AppProvider>.');
   }
-
+  const handlePermission = async () => {
+    const response = await getUserInfo();
+    if (response == 500) {
+      localStorage.removeItem('token');
+      Router.push('/login')
+    } else if (ctx.userDetails.userId) {
+      Router.push(`/profile/${ctx.userDetails.userId}`);
+    }
+  }
   return (
-    <Link href='/profile/[id]' as={`/profile/${ctx.userDetails && ctx.userDetails.userId}`}>
-      <a >
-        Profile
+    <a onClick={handlePermission} className={style.list}>
+      Profil
     </a>
-    </Link>
   )
 }
