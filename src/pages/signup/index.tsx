c072793ea36e7f032ea7beb498d03541bc78e05c
Your Name <you@example.com>
2020-10-10 18:34:57 +0300

    added signup translation

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
 import Router from "next/router";
 import { Form, Input, Button, Cascader } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import style from "./signup.module.css";
 import { locationOptions } from "utils/locationOptions";
+import { ContextProps, AppContext } from "context/AppContext";
 
 interface Value {
   email: string;
@@ -16,6 +17,10 @@ interface Value {
 }
 
 const Signup = () => {
+  const ctx: ContextProps | null = useContext(AppContext);
+  if (!ctx) {
+    throw new Error('You probably forgot to put <AppProvider>.');
+  }
   const [status, setStatus] = useState<Status>("pending");
 
   const auth = async (value: Value) => {
@@ -30,6 +35,7 @@ const Signup = () => {
       });
       localStorage.setItem("token", res.data.token);
       setStatus("success");
+      await ctx.setUserInfo()
       Router.push("/");
     } catch (err) {
       setStatus("error");
@@ -46,7 +52,7 @@ const Signup = () => {
       >
         <Form.Item
           name="email"
-          rules={[{ required: true, message: "Please input your email!" }]}
+          rules={[{ required: true, message: "Introdu email!" }]}
         >
           <Input
             prefix={<UserOutlined className="site-form-item-icon" />}
@@ -55,47 +61,48 @@ const Signup = () => {
         </Form.Item>
         <Form.Item
           name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
+          rules={[{ required: true, message: "Introdu numele utilizator" }]}
         >
           <Input
             prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="username"
+            placeholder="nume utilizator"
           />
         </Form.Item>
         <Form.Item
-          label="Location"
+          label="Locația"
           name="location"
-          rules={[{ required: true, message: "Please input your Location" }]}
+          rules={[{ required: true, message: "Introdu locația" }]}
         >
           <Cascader
             options={locationOptions}
+            placeholder="Selectează"
           />
         </Form.Item>
         <Form.Item
           name="title"
-          rules={[{ required: true, message: "Please input your profile title!" }]}
+          rules={[{ required: true, message: "Introdu numele profilului" }]}
         >
           <Input
-            placeholder="Profile title"
+            placeholder="Numele profilului"
           />
         </Form.Item>
         <Form.Item
           name="description"
-          rules={[{ required: true, message: "Please input your profile description!" }]}
+          rules={[{ required: true, message: "Introdu descriptia de profil" }]}
         >
           <Input.TextArea
             autoSize={true}
-            placeholder="Profile description"
+            placeholder="Descripția profilului"
           />
         </Form.Item>
         <Form.Item
           name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
+          rules={[{ required: true, message: "Introdu parola" }]}
         >
           <Input
             prefix={<LockOutlined className="site-form-item-icon" />}
             type="password"
-            placeholder="Password"
+            placeholder="Parolă"
           />
         </Form.Item>
         <Form.Item>
@@ -104,7 +111,7 @@ const Signup = () => {
             htmlType="submit"
             className={style.signupButton}
           >
-            Sign up
+            Înregistrare
           </Button>
         </Form.Item>
         {status === "error" && (
